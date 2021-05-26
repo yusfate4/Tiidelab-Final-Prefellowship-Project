ecb7189b671812da2546dcf8129c9bcfd7e9b081
Dahud Yusuf Ishola <abiodunyusuf4@gmail.com>
2021-05-26 03:46:54 +0100

    first commit

---
+++ assets/js/hamburger.js
@@ -0,0 +1,9 @@
+// get access to the nav item container from the HTML
+const navItemContainer = document.getElementById("nav-item-wrapper")
+
+
+
+// function that opens and closes the nav barf
+function toggleNavbar(){
+    navItemContainer.classList.toggle("open-nav-items")
+}
